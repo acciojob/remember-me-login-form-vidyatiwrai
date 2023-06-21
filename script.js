9a9e104565298e054741d70f10bc29bf706367e6
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-21 09:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+   function login() {
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+      const rememberMe = document.getElementById('checkbox').checked;
+
+      if (rememberMe) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+      } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+      }
+
+      alert(`Logged in as ${username}`);
+    }
+
+    function loginAsExistingUser() {
+      const savedUsername = localStorage.getItem('username');
+      alert(`Logged in as ${savedUsername}`);
+    }
